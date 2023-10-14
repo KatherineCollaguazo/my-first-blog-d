cfb31dffab532ab13009d73d04b52ed73652781a
KatherineCollaguazo <katherinealexandracollaguazo@gmail.com>
2023-10-14 14:57:00 -0500

    First commit

---
+++ funciones.py
@@ -0,0 +1,4 @@
+def fun1(a, b):
+    print(a*b)
+def hi(name):
+    print('Hi ' + name + '!')
---
+++ programa1.py
@@ -0,0 +1,13 @@
+from funciones import fun1, hi
+var1 = 5
+var2 = 9
+
+""" if var1 > var2:
+    print("Es correcto")
+else:
+    fun1(3 , 5) """
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
